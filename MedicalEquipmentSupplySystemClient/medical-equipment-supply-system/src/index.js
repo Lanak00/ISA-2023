e11dd3941a679b7d72dd9f3631596fb85696e60c
Lanak00 <kovaceviclana2501@gmail.com>
2023-11-18 18:01:06 +0100

    fetched data about companies

--- MedicalEquipmentSupplySystemClient/medical-equipment-supply-system/src/index.js
+++ MedicalEquipmentSupplySystemClient/medical-equipment-supply-system/src/index.js
@@ -5,6 +5,7 @@ import {BrowserRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
 
+const cors = require('cors');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- MedicalEquipmentSupplySystemClient/medical-equipment-supply-system/src/pages/SupplyCompanies.js
+++ MedicalEquipmentSupplySystemClient/medical-equipment-supply-system/src/pages/SupplyCompanies.js
@@ -1,42 +1,38 @@
+import { useState, useEffect } from "react";
+
 import SupplyCompanyList from "../components/companies/SupplyCompanyList";
 
-const DUMMY_DATA = [
-    {
-        id : 1,
-        name: 'Supply Company 1',
-        address: 'Bulevar Oslobodjenja 2',
-        city: 'Backa Palanka',
-        rating: 4.3,
-        image: 'https://upload.wikimedia.org/wikipedia/commons/3/3d/Hartford_Hospital_main_entrance.JPG',
-        description: 'Pharmaceutical Information Centre has changed its name to Pharmaca Health Intelligence Ltd and renews its brand to better serve its customers in Finland and internationally. The new name and brand support the company’s internationalization goals and clarify its service offering'
-        
-    },
-  
-    {
-        id : 2,
-        name: 'Supply Company 2',
-        address: 'Blagoja Parovica 81',
-        city: 'Backa Palanka',
-        rating: 4.3,
-        image: 'https://upload.wikimedia.org/wikipedia/commons/a/a2/Biandintz_eta_zaldiak_-_modified2.jpg',
-        description: 'Pharmaceutical Information Centre has changed its name to Pharmaca Health Intelligence Ltd and renews its brand to better serve its customers in Finland and internationally. The new name and brand support the company’s internationalization goals and clarify its service offering. '
-    },
-    {
-        id : 3,
-        name: 'Supply Company 3',
-        address: 'Kosovska 18',
-        city: 'Leskovac',
-        rating: 4.9,
-        image: 'https://upload.wikimedia.org/wikipedia/commons/3/30/Jubilee_and_Munin%2C_Ravens%2C_Tower_of_London_2016-04-30.jpg',
-        description: 'Pharmaceutical Information Centre has traveled a great and growth-oriented journey. Pharmaca Health Intelligence Ltd, more familiarly Pharmaca, is a tribute to nearly 50 years of development work. Not to forget Pharmaca Fennica®’s pharmaceutical information, which will continue to be the heart of the business also in the future '
-    }
-]
 
 function AllSupplyCompaniesPage() {
+const [isLoading, setIsLoading] = useState(true);
+const [loadedCompanies, setLoadedCompanies] = useState([]);
+
+useEffect(() => {
+    setIsLoading(true);
+    fetch(
+        'https://localhost:7260/supplyCompanies'
+    ).then((response) => {
+       return response.json();
+    }).then((data) => {
+        setIsLoading(false);
+        setLoadedCompanies(data);
+        console.log(data);
+    });
+}, []);
+
+   
+    if(isLoading) {
+        return (
+            <section>
+                <p>Loading...</p>
+            </section>
+        );
+    }
 
     return (
         <section>
-          <SupplyCompanyList companies = {DUMMY_DATA}/>
+          <SupplyCompanyList companies = {loadedCompanies}/>
+          
         </section>
     );
 }
